f54cd82b22af60022bf5376619acd40421b5985b
wangding0421 <wangding0421@gmail.com>
2016-02-06 21:28:32 -0800

    Changed test dimension

--- cse253hw3/test.py
+++ cse253hw3/test.py
@@ -47,26 +47,26 @@ if __name__ == '__main__':
 tr_N = len(tr_flabels);
 te_N = len(te_flabels);
 
-X = np.zeros((te_N, 32, 32, 3), dtype=np.uint8)
-y = np.zeros(te_N, dtype=np.int64)
+X = np.zeros((tr_N, 3, 32, 32), dtype=np.uint8)
+y = np.zeros(tr_N, dtype=np.int64)
 
-for i in range(te_N):
-    X[i] = np.rollaxis(te_data[i].reshape((3,32,32)),0,3)
-    y[i] = te_flabels[i]
+for i in range(tr_N):
+    X[i] = tr_data[i].reshape((3,32,32))
+    y[i] = tr_flabels[i]
 
 mapSize1 = X.nbytes * 10
-env = lmdb.open('test_data_lmdb', map_size=mapSize1)
+env = lmdb.open('train_data_lmdb', map_size=mapSize1)
 
 with env.begin(write=True) as txn:
     # txn is a Transaction object
-    for i in range(te_N):
+    for i in range(tr_N):
         datum = caffe.proto.caffe_pb2.Datum()
-        datum.channels = X.shape[3]
-        datum.height = X.shape[1]
-        datum.width = X.shape[2]
+        datum.channels = X.shape[1]
+        datum.height = X.shape[2]
+        datum.width = X.shape[3]
         datum.data = X[i].tobytes()  # or .tostring() if numpy < 1.9
         datum.label = int(y[i])
         str_id = '{:08}'.format(i)
 
         # The encode is only essential in Python 3
-        txn.put(str_id.encode('ascii'), datum.SerializeToString())
+        #txn.put(str_id.encode('ascii'), datum.SerializeToString())
